d05c90b3ad8181b567312d1933f811c4d8e687ad
wguevara11 <90358816+wguevara11@users.noreply.github.com>
2021-10-21 15:37:45 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <html>
+      <head>
+        <title> Westin's Resume</title>
+      </head>
+      <div>
+        <h1>Hello, my name is Westin!</h1>
+        <p>This website was specifically made for my resume.</p>
+        <p>
+          Contact Information: (516)-770-4524 personal email:
+          westinguevara@gmail.com school email: wguevara@lions.molloy.edu
+        </p>
+        <h3> Molloy College (September 2021-present)</h3>
+        <ul>
+          <li>Major in Computer Science</li>
+        </ul>
+        <h3> High School Activities (2018-2021)</h3>
+        <ul>
+          <li>National Science Society(2019-2021)</li>
+          <li>Red Cross Club(2020-2021)</li>
+          <li>Student Government(2021)</li>
+        </ul>
+        <h3>Athletics</h3>
+        <ul>
+          <li> Boys Varsity Soccer (2017-2020)</li>
+          <li> Boys Varisty Wreslting (2017-2021)</li>
+        </ul>
+        <h3>Awards and Recognition</h3>
+        <ul>
+          <li> Top 20 in Graduating Class</li>
+          <li> High Honor Roll(2018-2021)</li>
+          <li> Top 10 Most Athletic (2018)</li>
+          <li> All Conference Boys Varisty Soccer(2020)</li>
+        </ul>
+        <h3>Employment</h3>
+        <ul>
+          <li> Dan's Bouncy House Company (2016-2018)</li>
+          <li> Burger King (2018-2020)</li>
+          <li> Cafe Baci (2018-present)</li>
+        </ul>
+      </div>
+    </html>
+  );
+}
